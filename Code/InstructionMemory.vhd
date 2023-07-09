04af25727b9931aa06512eca3ec124d7b89b0941
Frosso Varsou <frossovarsou@gmail.com>
2023-07-09 20:40:01 +0300

    Update InstructionMemory.vhd

--- Code/InstructionMemory.vhd
+++ Code/InstructionMemory.vhd
@@ -13,17 +13,17 @@ end InstructionMemory;
 architecture behavioral of InstructionMemory is
 
 	type ROM_16_x_32 is array (0 to 15) of std_logic_vector(31 downto 0);
-	signal IM : ROM_16_x_32 := (x"20000000", --addi $0, $0, 0 
-								x"20420000", --addi $2, $2, 0
-								x"20820000", --addi $2, $4, 0
-								x"20030001", --addi $3, $0, 1
-								x"20050003", --addi $5, $0, 3
-								x"00603020", --L1: add $6, $3, $0
-								x"AC860000", --sw $6, 0($4)
-								x"20630001", --addi $3, $3, 1
-								x"20840001", --addi $4, $4, 1
-								x"20A5FFFF", --addi $5, $5, -1
-								x"14A0FFFA", --*bne $5,$0,L1 !!
+	signal IM : ROM_16_x_32 := (x"20000000",
+								x"20420000", 
+								x"20820000",
+								x"20030001", 
+								x"20050003", 
+								x"00603020", 
+								x"AC860000", 
+								x"20630001", 
+								x"20840001", 
+								x"20A5FFFF", 
+								x"14A0FFFA", 
 								x"00000000",
 								x"00000000",
 								x"00000000",
